8f0b47e5eaa920db8c401785ed4a5925a60bcb84
Yasho Bapat <93445114+Yasho-Bapat@users.noreply.github.com>
2022-10-18 01:10:45 +0530

    Add files via upload

---
+++ tempav.py
@@ -0,0 +1,11 @@
+import Adafruit_DHT as dht
+import time
+sensor = dht.DHT22
+pin = 4
+
+temp_list = list()
+for i in range(25):
+    hum, temp = dht.read_retry(sensor, pin)
+    temp_list.append(temp)
+
+print("average temperature over 25 readings = " + (sum(temp_list)/len(temp_list)))
