284977b0d073a6faae2c60b6e6ebde6ff9f0d7c8
Yasho Bapat <93445114+Yasho-Bapat@users.noreply.github.com>
2022-10-21 15:14:03 +0530

    Update basicscript.py

--- basicscript.py
+++ basicscript.py
@@ -4,7 +4,7 @@ import time
 sensor = dht.DHT22
 pin = 4
 
-for i in range 5:
+for i in range (5):
   hum, temp = dht.read_retry(sensor, pin)
   print("Humidity: " + str(hum) + "; Temeperature: " + str(temp))
   time.sleep(2)
